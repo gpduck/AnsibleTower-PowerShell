d7342764c1d9092a1af1aef86d8f71c5ff5e3bde
rvo <rodric@vosenterprises.eu>
2017-03-06 21:20:10 +0100

    Adapted for Ansible 3
    - Added comment based help in AnsibleTower.psm1 and
    AnsibleJobTemplate.ps1.
    - Added function Get-AnsibleJobTemplateID
    - Added function Join-AnsibleUrl
    - Added function Get-AnsibleResourceUrl
    - Specify job template name or id to the *-AnsibleJobTemplate*
    functions.
    - Added more field to the strong types User, JobTemplate and Job.
    - Connect-AnsibleTower returns the user as a strongly typed object.
    - Enhanced or added error handling.
    - Bumped module version from 0.1 to 0.2.

--- AnsibleJobTemplate.ps1
+++ AnsibleJobTemplate.ps1
@@ -1,75 +1,138 @@
+function Get-AnsibleJobTemplateID
+{
+    <#
+    .SYNOPSIS
+    Gets the job template ID from a job template name.
+
+    .EXAMPLE
+    Get-AnsibleJobTemplateID -Name 'Demo Job Template'
+
+    .EXAMPLE
+    'Demo Job Template' | Get-AnsibleJobTemplateID
+
+    .OUTPUTS
+    The job ID.
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,Position=0)]
+        [string]$Name
+    )
+
+    (Get-AnsibleJobTemplate | ? { $_.name -eq $Name }).id
+}
+
 function Get-AnsibleJobTemplate
 {
     <#
-        .SYNOPSIS
+    .SYNOPSIS
+    Gets one or all job templates.
 
-        Gets one or multiple Job Templates
+    .EXAMPLE
+    Get-AnsibleJobTemplate
 
+    Gets all job templates.
+
+    .EXAMPLE
+    Get-AnsibleJobTemplate | where { $_.project -eq 4 }
+
+    Gets all job templates that belong to project ID 4.
+
+    .EXAMPLE
+    Get-AnsibleJobTemplate 'Demo Job Template'
+
+    Gets details about job template named 'Demo Job Template'.
+
+    .EXAMPLE
+    $jobTemplate = Get-AnsibleJobTemplate -ID 5
+
+    .OUPUTS
+    Strongly typed job template object(s).
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Name')]
     Param (
-        [Parameter(ValueFromPipelineByPropertyName=$true)]
-        [int]$id
+        [Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,Position=0,ParameterSetName='Name')]
+        [string]$Name,
+
+        [Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,Position=0,ParameterSetName='ID')]
+        [int]$ID
     )
 
-    if ($id)
-    {
-        $Return = Invoke-GetAnsibleInternalJsonResult -ItemType "job_templates" -Id $id
+    if ($Name) {
+        $ID = Get-AnsibleJobTemplateID -Name $Name
+        if (!$ID) {
+            throw ("Failed to get the ID for job template named [{0}]" -f $Name)
+        }
     }
-    Else
-    {
-        $Return = Invoke-GetAnsibleInternalJsonResult -ItemType "job_templates"
-    }
-    
 
-    if (!($Return))
-    {
-        #Nothing returned from the call
-        Return
+    if ($ID) {
+        $return = Invoke-GetAnsibleInternalJsonResult -ItemType "job_templates" -Id $ID
+    } else {
+        $return = Invoke-GetAnsibleInternalJsonResult -ItemType "job_templates"
     }
-    $returnobj = @()
+
+    if (!$return)
+    {
+        # Nothing returned from the call
+        return
+    }
+
+    $returnObjs = @()
     foreach ($jsonorg in $return)
     {
-        #Shift back to json and let newtonsoft parse it to a strongly named object instead
+        # Shift back to json and let newtonsoft parse it to a strongly named object instead
         $jsonorgstring = $jsonorg | ConvertTo-Json
         $org = $JsonParsers.ParseToJobTemplate($jsonorgstring)
-        $returnobj += $org; $org = $null
+        $returnObjs += $org;
+        $org = $null
 
     }
     #return the things
-    $returnobj
+    $returnObjs
 }
 
 
 function Invoke-AnsibleJobTemplate
 {
     <#
-        .SYNOPSIS
+    .SYNOPSIS
+    Runs an Ansible job template.
 
-        Invokes an Ansible Job Template
+    .EXAMPLE
+    Invoke-AnsibleJobTemplate -Name 'Demo Job Template'
 
-        .EXAMPLE
+    Runs a job for job template named 'Demo Job Template'.
 
-        Connect-AnsibleTower -Credential (get-credential) -TowerUrl "https://mytower" -DisableCertificateVerification
-        $jobtemplate = get-ansibleJobTemplate -id 5
-        $jobtemplate | Invoke-AnsibleJobTemplate
+    .EXAMPLE
+    $job = Invoke-AnsibleJobTemplate -ID 5
 
+    Runs a job for job template with ID 5.
+
+    .OUTPUTS
+    Strongly typed job object.
     #>
 
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Name')]
     Param (
-        [Parameter(ValueFromPipelineByPropertyName=$true,Mandatory=$true)]
-        [int]$id
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,Position=0,ParameterSetName='Name')]
+        [string]$Name,
+
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,Position=0,ParameterSetName='ID')]
+        [int]$ID
     )
 
-    $ThisJobTemplate = Get-AnsibleJobTemplate -id $id
+    if ($Name) {
+        $ID = Get-AnsibleJobTemplateID -Name $Name
+        if (!$ID) {
+            throw ("Failed to get the ID for job template named [{0}]" -f $Name)
+        }
+    }
 
-    if (!$ThisJobTemplate) {Write-Error "No Job template with id $id"; return}
-
-    $result = Invoke-PostAnsibleInternalJsonResult -ItemType "job_templates" -itemId $id -ItemSubItem "jobs"
-    $JobId = $result.id
-    $job = get-ansibleJob -id $JobId
-    $job
+    $result = Invoke-PostAnsibleInternalJsonResult -ItemType "job_templates" -itemId $ID -ItemSubItem "launch"
+    if (!$result -and !$result.id) {
+        throw ("Failed to start job for job template ID [{0}]" -f $ID);
+    }
+    $jobId = $result.id
+    Get-AnsibleJob -id $jobId
 }
-
-
