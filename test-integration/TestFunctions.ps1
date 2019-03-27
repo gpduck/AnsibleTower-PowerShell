927938f2cf9b3bb9a8167b863577fca903d65531
Chris Duck <gpduck@whatsupduck.net>
2019-03-27 00:00:43 -0500

    Try reconnecting to fix host test errors

--- test-integration/TestFunctions.ps1
+++ test-integration/TestFunctions.ps1
@@ -13,3 +13,11 @@ function Reload-AWX {
 function Get-RandomString {
     [Guid]::NewGuid().ToString().Substring(0, 25)
 }
+
+function Get-AnsibleCredential {
+    param(
+        $Password
+    )
+    $PasswordSS = ConvertTo-SecureString -ASPlainText $Password -Force
+    New-Object System.Management.Automation.PSCredential("admin", $PasswordSS)
+}
